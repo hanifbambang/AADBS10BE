9770d5bb15ecd21720a773b0825e8c7e2cd3604c
Hanif <hanifbambang30@gmail.com>
2026-05-07 21:46:38 +0700

    update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'https://aadbs-10-fe.vercel.app',
+  origin: process.env.CLIENT_URL || 'https://aadbs-10-fe.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
